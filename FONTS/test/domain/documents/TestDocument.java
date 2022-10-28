b47b67e2e853c0e88d1cccfebf536acc59713cbf
Ariadna Cortés Danés <acd200111@gmail.com>
2022-10-28 11:54:23 +0200

    tests getters

--- FONTS/test/domain/documents/TestDocument.java
+++ FONTS/test/domain/documents/TestDocument.java
@@ -1,34 +1,44 @@
 package test.domain.documents;
 
+import java.util.*;
+import main.domain.documents.Document;
+
 //TestChecks
 import static org.junit.Assert.*;
 
-import main.domain.documents.Document;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class TestDocument {
     
+    @BeforeClass public static void init(){
+    }
+
     /**
 	 * Test dels getters
 	 */
     @Test
-	public void GetterTesting() throws Exception
+	public void testGetters()
 	{
-        Document doc = new Document("Ari", "Titol del document", "Aixo es el contingut", "txt");
-        assertEquals("Ari", doc.getAuthor());
-        assertEquals("Titol del document", doc.getTitle());
-        assertEquals("Aixo es el contingut", doc.getContent());
-        assertEquals("txt", doc.getOriginalFormat());
+        //test els getters per als diferents tipus de constructores
+        Document doc1 = new Document("Ari", "Titol del document", "Contingut", "txt");
+        assertEquals("Ari", doc1.getAuthor());
+        assertEquals("Titol del document", doc1.getTitle());
+        assertEquals("Contingut", doc1.getContent());
+        assertEquals("txt", doc1.getOriginalFormat());
 
-        //getter del internal document :)
-
+        Document doc2 = new Document("Ari", "Titol del document", "Contingut");
+        assertEquals("Ari", doc2.getAuthor());
+        assertEquals("Titol del document", doc2.getTitle());
+        assertEquals("Contingut", doc2.getContent());
+        assertEquals(null, doc2.getOriginalFormat());
     }
 
     /**
 	 * Test dels setters
 	 */
     @Test
-	public void SetterTesting() throws Exception
+	public void testSetters()
 	{
         Document doc = new Document("Ari", "Titol del document", "Aixo es el contingut", "txt");
         doc.setContent("Now content :)");
@@ -39,10 +49,50 @@ public class TestDocument {
 	 * Test dels setters
 	 */
     @Test
-	public void queryRelevanceTesting() throws Exception
+	public void testTermRelevance_tf_idf()
 	{
-        Document doc = new Document("Ari", "Titol del document", "furbo furbo unga unga", "txt");
+        int num_docs = 10;
+        Map<String,Integer> presence = new HashMap<String,Integer>();
+        presence.put("Tres", 7);
+        presence.put("tristes", 2);
+        presence.put("tigres", 1);
+        presence.put("comen", 3);
+        presence.put("trigo", 2);
+        presence.put("en", 6);
+        presence.put("un", 9);
+        presence.put("trigal", 1);
+        presence.put("test", 10);
+        presence.put("mesTest", 9);
+        presence.put("paraulaaa", 9);
+        presence.put("parauula", 9);
+        Document doc = new Document("Ari", "Titol del document", "Tres tristes tigres comen trigo en un trigal.", "txt");
+        String[] words = {"tigres","trigo"};
+
+        assertEquals(25.0, doc.termRelevance_tf_idf(words,num_docs,presence),0.1);
+    }
+    
+    /**
+	 * Test dels setters
+	 */
+    @Test
+	public void testQueryRelevance()
+	{
+        int num_docs = 10;
+        Map<String,Integer> presence = new HashMap<String,Integer>();
+        presence.put("tres", 7);
+        presence.put("tristes", 2);
+        presence.put("tigres", 1);
+        presence.put("comen", 3);
+        presence.put("trigo", 2);
+        presence.put("en", 6);
+        presence.put("un", 9);
+        presence.put("trigal", 1);
+        presence.put("test", 10);
+        presence.put("mesTest", 9);
+        presence.put("paraulaaa", 9);
+        presence.put("parauula", 9);
+        Document doc = new Document("Ari", "Titol del document", "Tres tristes tigres comen trigo en un trigal", "txt");
         String query = "me gusta el furbo";
-        assertEquals(25, doc.queryRelevance(query));
+        assertEquals(25.0, doc.queryRelevance(query,num_docs,presence),0.1);
     }
 }
--- FONTS/test/domain/documents/TestInternalDocument.java
+++ FONTS/test/domain/documents/TestInternalDocument.java
@@ -1,4 +1,59 @@
 package test.domain.documents;
 
+import main.domain.documents.InternalDocument;
+import java.util.*;
+
+//TestChecks
+import static org.junit.Assert.*;
+
+import org.junit.Test;
 public class TestInternalDocument {
+
+    /**
+     * Few tests on the split method
+     */
+    @Test
+    public void testSplit() {
+        String c1 = "Tres,tristes, tigres, ";
+        String[] s1 = c1.split("[ ,!?.]+");
+        String[] expectedResult = {"Tres", "tristes", "tigres"};
+        assertArrayEquals(expectedResult, s1);
+
+        String c2 = "Tres!tristes ! tigres! ";
+        String[] s2 = c2.split("[- ,!?.]+");
+        assertArrayEquals(expectedResult, s2);
+
+        String c3 = "Tres?tristes ? tigres? ";
+        String[] s3 = c3.split("[- ,!?.]+");
+        assertArrayEquals(expectedResult, s3);
+
+        String c4 = "Tres. -.- tristes?. tigres...";
+        String[] s4 = c4.split("[- ,!?.]+");
+        assertArrayEquals(expectedResult, s4);
+        
+        String c5 = "Tres-tristes-tigres?! -";
+        String[] s5 = c5.split("[- ,!?.]+");
+        assertArrayEquals(expectedResult, s5);
+    }
+    /**
+     * @brief Getter testing
+     */
+    @Test
+    public void testGetters() {
+        String content = "Tres tristes tigres comen trigo en un trigal.";
+        InternalDocument internal1 = new InternalDocument(content);
+        Set<String> set = internal1.getRelevantKeyWords();
+
+        assertEquals(8, set.size());
+        assertTrue(set.contains("Tres"));
+        assertTrue(set.contains("tristes"));
+        assertTrue(set.contains("tigres"));
+        assertTrue(set.contains("comen"));
+        assertTrue(set.contains("trigo"));
+        assertTrue(set.contains("en"));
+        assertTrue(set.contains("un"));
+        assertTrue(set.contains("trigal"));
+
+        assertEquals(8, internal1.getTotalWords());
+    }
 }
