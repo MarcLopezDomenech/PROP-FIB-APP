d2d28b94e302a1a2eadd78cfc0b4fb6a488027e7
mvalls21 <marc.valls.camps@estudiantat.upc.edu>
2022-11-12 15:28:28 +0100

    [EXPRESSION] Bugfix: hola ! (pt 2) pero bonito

--- FONTS/main/domain/expressions/Expression.java
+++ FONTS/main/domain/expressions/Expression.java
@@ -177,10 +177,9 @@ public abstract class Expression {
             return new Not(recursiveDeconstruction(inner));
         }
 
-        for (int k = 1; k < expression.length() - 1; ++k) {
+        for (int k = 1; k < expression.length() - 1; ++k)
             if (expression.charAt(k) == '"' || expression.charAt(k) == '!') throw new ExceptionInvalidExpression(expression);
-        }
-        if (expression.charAt(0) == '!' || expression.charAt(expression.length()-1) == '!') throw new ExceptionInvalidExpression(expression);
+        if (expression.charAt(expression.length()-1) == '!') throw new ExceptionInvalidExpression(expression);
 
         if (expression.charAt(0) == '"') {
             if (expression.charAt(expression.length()-1) != '"') throw new ExceptionInvalidExpression(expression);
