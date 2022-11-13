ab1f0d591d74f4cec275929eede6e7f47a03a138
Pau Duran <pau.duran.manzano@upc.edu>
2022-11-13 11:26:28 +0100

    Stubs de True i False, tests de And, Or i Not

--- FONTS/test/domain/expressions/TestOr.java
+++ FONTS/test/domain/expressions/TestOr.java
@@ -1,6 +1,7 @@
 package test.domain.expressions;
 
 import main.domain.expressions.Or;
+import main.excepcions.ExceptionInvalidExpression;
 import test.domain.expressions.Expression;
 import org.junit.Test;
 
@@ -10,23 +11,23 @@ import static org.junit.Assert.assertTrue;
 /**
  * @class TestOr
  * @brief Classe per provar de forma unitària la classe Or
- * @author marc.valls.camps
+ * @author marc.valls.camps i pau.duran.manzano
  */
 
 public class TestOr {
     @Test
-    public void basic_constructor() {
-        Expression e1 = Expression.create("true");
-        Expression e2 = Expression.create("false");
-        Or o1 = new Or(e1, e1);
-        Or o2 = new Or(e1, e2);
-        Or o3 = new Or(e2, e1);
-        Or o4 = new Or(e2, e2);
+    public void basic_constructor() throws ExceptionInvalidExpression {
+        Expression evalTrue = Expression.create("true");
+        Expression evalFalse = Expression.create("false");
+        Or tt = new Or(evalTrue, evalTrue);
+        Or tf = new Or(evalTrue, evalFalse);
+        Or ft = new Or(evalFalse, evalTrue);
+        Or ff = new Or(evalFalse, evalFalse);
 
         // es prova tota la taula de veritat de la connectiva
-        assertTrue(o1.evaluate("content", false));
-        assertTrue(o2.evaluate("content", false));
-        assertTrue(o3.evaluate("content", false));
-        assertFalse(o4.evaluate("content", false));
+        assertTrue(tt.evaluate("content", false));
+        assertTrue(tf.evaluate("content", false));
+        assertTrue(ft.evaluate("content", false));
+        assertFalse(ff.evaluate("content", false));
     }
 }
