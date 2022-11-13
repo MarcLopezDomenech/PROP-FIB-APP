f67a0e97f83b9a3392a221335e4baa21d7f0361e
Pau Duran <pau.duran.manzano@upc.edu>
2022-11-13 09:45:08 +0100

    Amplicació stub Expression

--- FONTS/test/domain/expressions/Expression.java
+++ FONTS/test/domain/expressions/Expression.java
@@ -8,6 +8,7 @@ import main.excepcions.ExceptionInvalidExpression;
  * @author pau.duran.manzano
  */
 public class Expression {
+    private String forceEvaluation;
     /**
      * @brief Funció per imitar la creació d'una expressió
      * @details En la classe de veritat, Expression és abstracte i té aquest mètode per donar-ne d'alta una. Aquí el declarem també, amb la possbilitat de llençar l'excepció de invalid (una possibilitat en la classe de veritat) quan rebem el string "invalid", per poder fer proves
@@ -15,8 +16,9 @@ public class Expression {
      * @return Expressió booleana per poder provar, amb les funcionalitats bàsiques
      * @throws ExceptionInvalidExpression en cas que el string donat digui "invalid"
      */
-    public static Expression create(String expression) throws ExceptionInvalidExpression {
+    public Expression create(String expression) throws ExceptionInvalidExpression {
         if ("invalid".equals(expression)) throw new ExceptionInvalidExpression("prova");
+        forceEvaluation = expression;
         return new Expression();
     }
 
@@ -25,9 +27,11 @@ public class Expression {
      * @details Implementem un mètode per poder fer tests que vulguin avaluar un contingut a partir d'una expressió booleana
      * @param content amb el contingut que es voldria avaluar
      * @param caseSensitive indicant si caldria fer una avaluació case sensitive o no
-     * @return Retorna cert si el contingut donat és "true", altrament retorna fals.
+     * @return Retorna cert si en la creació s'havia donat com expression "true", fals si en la creació el contingut era "false". Altrament, retorna cert o fals en funció de si el contingut té una llargada major o menor/igual a 5, respectivament.
      */
     public Boolean evaluate(String content, Boolean caseSensitive) {
-        return content.length() > 5;
+        if ("true".equals(forceEvaluation)) return true;
+        else if ("false".equals(forceEvaluation)) return false;
+        else return content.length() > 5;
     }
 }
