5efa94cd0b75f06b3e87c4423e12aaa8059d2b42
Ariadna Cortés Danés <acd200111@gmail.com>
2022-11-14 08:48:13 +0100

    .bat aca(bat)

--- FONTS/compila.bat
+++ FONTS/compila.bat
@@ -1,8 +1,8 @@
 @echo off
 
-REM set argC=0
-REM for %%x in (%*) do Set /A argC+=1
-REM if NOT argC == 1 goto :error
+set argC=0
+for %%x in (%*) do Set argC=1
+if %argC% == 0 goto :error
 
 if /i %1 == TestAnd goto :and
 if /i %1 == Driver goto :driver
@@ -16,7 +16,7 @@ if /i %1 == TestNot goto :nota
 if /i %1 == TestOr goto :or
 if /i %1 == TestPair goto :pair
 
-REM :error
+:error
 echo:
 echo USAGE
 echo compila.bat test-a-executar
@@ -43,13 +43,16 @@ REM === AND ===
 javac -cp ".;../lib/junit-4.12.jar;../hamcrest-core-1.3.jar" -d ../EXE/And ./test/domain/expressions/TestAnd.java
 
 :and
+echo Running test for And...
 java -cp "../EXE/And;./../lib/junit-4.12.jar;./../lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore test.domain.expressions.TestAnd
 goto :EOF
 
 REM === DRIVER INTERACTIU ===
 :driver
+echo Recompiling Driver...
 javac -cp "." -d ../EXE/CtrlDomain ./test/domain/DriverCtrlDomain.java
 
+echo Running Driver...
 java -cp ../EXE/CtrlDomain test.domain.DriverCtrlDomain
 goto :EOF
 
@@ -57,6 +60,7 @@ REM === DOCUMENT ===
 javac -cp ".;../lib/junit-4.12.jar;../hamcrest-core-1.3.jar" -d ../EXE/Document test/domain/documents/TestDocument.java
 
 :document
+echo Running test for Document...
 java -cp "../EXE/Document;../lib/junit-4.12.jar;../lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore test.domain.documents.TestDocument
 goto :EOF
 
@@ -65,6 +69,7 @@ REM === DOCUMENTSSET ===
 javac -cp ".;../lib/junit-4.12.jar;../hamcrest-core-1.3.jar" -d ../EXE/DocumentSet test/domain/documents/TestDocumentsSet.java
 
 :documentsset
+echo Running test for DocumentsSet...
 java -cp "../EXE/DocumentsSet;../lib/junit-4.12.jar;../lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore test.domain.documents.TestDocumentsSet
 goto :EOF
 
@@ -73,6 +78,7 @@ REM === EXPRESSION ===
 javac -cp ".;../lib/junit-4.12.jar;../hamcrest-core-1.3.jar" -d ../EXE/Expression test/domain/expressions/TestExpression.java
 
 :expression
+echo Running test for Expression...
 java -cp "../EXE/Expression;../lib/junit-4.12.jar;../lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore test.domain.expressions.TestExpression
 goto :EOF
 
@@ -81,6 +87,7 @@ REM === EXPRESSIONSSET ===
 javac -cp ".;../lib/junit-4.12.jar;../hamcrest-core-1.3.jar" -d ../EXE/ExpressionsSet test/domain/expressions/TestExpressionsSet.java
 
 :expressionsset
+echo Running test for ExpressionSet...
 java -cp "../EXE/ExpressionsSet;../lib/junit-4.12.jar;../lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore test.domain.expressions.TestExpressionsSet
 goto :EOF
 
@@ -89,6 +96,7 @@ REM === INTERNALDOCUMENT ===
 javac -cp ".;../lib/junit-4.12.jar;../hamcrest-core-1.3.jar" -d ../EXE/InternalDocument test/domain/documents/TestInternalDocument.java
 
 :internaldocument
+echo Running test for InternalDocument...
 java -cp "../EXE/InternalDocument;../lib/junit-4.12.jar;../lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore test.domain.documents.TestInternalDocument
 goto :EOF
 
@@ -97,6 +105,7 @@ REM === LITERAL ===
 javac -cp ".;../lib/junit-4.12.jar;../hamcrest-core-1.3.jar" -d ../EXE/Literal test/domain/expressions/TestLiteral.java
 
 :literal
+echo Running test for Literal...
 java -cp "../EXE/Literal;../lib/junit-4.12.jar;../lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore test.domain.expressions.TestLiteral
 goto :EOF
 
@@ -105,6 +114,7 @@ REM === NOT ===
 javac -cp ".;../lib/junit-4.12.jar;../hamcrest-core-1.3.jar" -d ../EXE/Not test/domain/expressions/TestNot.java
 
 :nota
+echo Running test for Not...
 java -cp "../EXE/Not;../lib/junit-4.12.jar;../lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore test.domain.expressions.TestNot
 goto :EOF
 
@@ -113,6 +123,7 @@ REM === OR ===
 javac -cp ".;../lib/junit-4.12.jar;../hamcrest-core-1.3.jar" -d ../EXE/Or test/domain/expressions/TestOr.java
 
 :or
+echo Running test for Or...
 java -cp "../EXE/Or;../lib/junit-4.12.jar;../lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore test.domain.expressions.TestOr
 goto :EOF
 
@@ -121,5 +132,6 @@ REM === PAIR ===
 javac -cp ".;../lib/junit-4.12.jar;../hamcrest-core-1.3.jar" -d ../EXE/Pair test/domain/util/TestPair.java
 
 :pair
+echo Running test for Pair...
 java -cp "../EXE/Pair;../lib/junit-4.12.jar;../lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore test.domain.util.TestPair
 goto :EOF
