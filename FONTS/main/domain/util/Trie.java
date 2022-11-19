bf893740f6427e535fe582f4cb7344e0a4c903d7
mvalls21 <marc.valls.camps@estudiantat.upc.edu>
2022-11-19 12:24:26 +0100

    [TRIE] Bugfixes, doxygen, test. ACABAT

--- FONTS/main/domain/util/Trie.java
+++ FONTS/main/domain/util/Trie.java
@@ -1,6 +1,7 @@
 package main.domain.util;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * @class Trie
@@ -8,72 +9,122 @@ import java.util.ArrayList;
  * @author marc.valls.camps
  */
 public class Trie {
+    /**
+     * @class Node
+     * @brief Nodes de l'arbre, cadascun representa un determinat prefix
+     * @author marc.valls.camps
+     */
     private class Node {
         /**
-         * \brief Primer camp de la parella
+         * \brief Nodes descendents, representant cadascun una lletra extenent el prefix del node pare
          */
-        public Node[] descendants;
+        public HashMap<Character,Node> descendants;
 
+        /**
+         * \brief Booleà que guarda si el prefix representat per aquest node és una paraula
+         */
         public boolean isEndWord;
 
+        /**
+         * @brief Constructora per defecte
+         * @return Una instància de Node inicialitzada amb els valors per defecte
+         */
         public Node() {
-            descendants = new Node[26]; // corregir
+            descendants = new HashMap<>();
             isEndWord = false;
         }
     }
 
     private Node t;
 
+    /**
+     * @brief Constructora per defecte
+     * @return Una instància de Trie amb només el Node arrel, representant el prefix buit
+     */
     public Trie(){
         t = new Node();
     }
 
-    public boolean insert(String s) {
+    /**
+     * @brief Recorre l'arbre fins al node que representa el prefix word
+     * @param word Paraula que s'ha de cercar en l'arbre
+     * @param create Booleà que indica si s'ha de crear els Nodes que faltin
+     * @return El Node a buscar si existia. Si no existia i el paràmetre create és cert aleshores s'ha creat i es retorna el nou Node,
+     * si era fals aleshores es retorna null.
+     */
+    private Node traverseToNode(String word, Boolean create) {
         Node n = t;
-        for (int i = 0; i < s.length(); ++i) {
-            if (n.descendants[s.charAt(i) - 'a'] == null) n.descendants[s.charAt(i) - 'a'] = new Node();
-            n = n.descendants[s.charAt(i) - 'a'];
+        for (int i = 0; i < word.length(); ++i) {
+            if (!n.descendants.containsKey(word.charAt(i))) {
+                if (create) n.descendants.put(word.charAt(i), new Node());
+                else return null;
+            }
+            n = n.descendants.get(word.charAt(i));
         }
+        return n;
+    }
+
+    /**
+     * @brief Inserció d'una paraula a l'arbre
+     * @param word Paraula a inserir
+     * @return Cert ssi no existia la paraula en l'arbre i s'ha pogut inserir correctament
+     */
+    public boolean insert(String word) {
+        Node n = traverseToNode(word, true);
         if (n.isEndWord) return false;
+
         n.isEndWord = true;
         return true;
     }
 
-    public boolean remove(String s) {
-        Node n = t;
-        for (int i = 0; i < s.length() - 1; ++i) {
-            if (n.descendants[s.charAt(i) - 'a'] == null) return false;
-            n = n.descendants[s.charAt(i) - 'a'];
-        }
-        if (n.descendants[s.charAt(s.length() - 1) - 'a'] == null) return false;
-        n.descendants[s.charAt(s.length() - 1) - 'a'] = null;
+    /**
+     * @brief Esborrat d'una paraula a l'arbre
+     * @param word Paraula a esborrar
+     * @return Cert ssi existia la paraula en l'arbre i s'ha pogut esborrar correctament
+     */
+    public boolean remove(String word) {
+        Node n = traverseToNode(word, false);
+        if (!n.isEndWord) return false;
+
+        n.isEndWord = false;
         return true;
     }
 
-    public boolean contains(String s) {
-        Node n = t;
-        for (int i = 0; i < s.length(); ++i) {
-            if (n.descendants[s.charAt(i) - 'a'] == null) return false;
-            n = n.descendants[s.charAt(i) - 'a'];
-        }
+    /**
+     * @brief Cerca d'una paraula en l'arbre
+     * @param word Paraula a buscar
+     * @return Cert ssi existia la paraula en l'arbre
+     */
+    public boolean contains(String word) {
+        Node n = traverseToNode(word, false);
         return n.isEndWord;
     }
 
+    /**
+     * @brief Cerca de totes les paraules donat un prefix
+     * @param prefix Prefix de les paraules a trobar
+     * @return El conjunt de les paraules de l'arbre que comencen amb el prefix especificat
+     */
     public ArrayList<String> wordsGivenPrefix(String prefix) {
-        Node n = t;
-        for (int i = 0; i < prefix.length(); ++i) {
-            if (n.descendants[prefix.charAt(i) - 'a'] == null) return new ArrayList<String>(0);
-            n = n.descendants[prefix.charAt(i) - 'a'];
-        }
+        Node n = traverseToNode(prefix, false);
         return recursiveCollect(prefix, n);
     }
 
-    private static ArrayList<String> recursiveCollect(String s, Node n) {
+    /**
+     * @brief Llistat de les paraules que pengen d'un Node
+     * @param prefix Prefix que s'afegirà a totes les paraules que es retornin
+     * @param n Node arrel de l'arbre a explorar
+     * @return Conjunt de les paraules que pengen del Node n, començant pel prefix donat
+     */
+    private static ArrayList<String> recursiveCollect(String prefix, Node n) {
         ArrayList<String> result = new ArrayList<>();
-        for (int i = 0; i < n.descendants.length; ++i)
-            result.addAll(recursiveCollect(s + (char)('a'+ i), n.descendants[i]));
+        if (n == null) return result;
 
-        if (n.isEndWord) result.add(s);
+        if (n.isEndWord) result.add(prefix);
+        for (java.util.Map.Entry<Character, Node> desc : n.descendants.entrySet()) {
+            result.addAll(recursiveCollect(prefix + desc.getKey(), desc.getValue()));
+        }
+
         return result;
     }
 }
--- FONTS/test/domain/util/TestTrie.java
+++ FONTS/test/domain/util/TestTrie.java
@@ -1,9 +1,12 @@
 package test.domain.util;
 
 import main.domain.util.Trie;
-import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
 
 /**
  * @class TestTrie
@@ -12,9 +15,42 @@ import static org.junit.Assert.assertTrue;
  */
 public class TestTrie {
     @Test
-    public void testTrie1() {
+    public void testInsertRemoveBoolean() {
         Trie t = new Trie();
-        t.insert("marc");
+        assertTrue(t.insert("marc"));
+        assertFalse(t.insert("marc"));
+
         assertTrue(t.contains("marc"));
+
+        assertTrue(t.remove("marc"));
+        assertFalse(t.remove("marc"));
+
+        assertFalse(t.contains("marc"));
+    }
+
+    @Test
+    public void testPrefixes() {
+        Trie t = new Trie();
+        assertTrue(t.insert("marc"));
+        assertTrue(t.insert("marta"));
+        assertTrue(t.insert("marcel"));
+
+        assertTrue(t.contains("marc"));
+        assertTrue(t.contains("marta"));
+        assertTrue(t.contains("marcel"));
+
+        ArrayList<String> result = new ArrayList<>(Arrays.asList("marc", "marcel"));
+        assertEquals(result,t.wordsGivenPrefix("marc"));
+
+        result.add("marta");
+        assertEquals(result,t.wordsGivenPrefix("mar"));
+        assertEquals(result,t.wordsGivenPrefix(""));
+
+        result.remove("marc");
+        assertTrue(t.remove("marc"));
+        assertEquals(result,t.wordsGivenPrefix("mar"));
+
+        result.remove("marta");
+        assertEquals(result,t.wordsGivenPrefix("marc"));
     }
 }
