efbf7987976dd35cd33e42a429ba4694dfa00c36
Pau Duran <pau.duran.manzano@upc.edu>
2022-12-10 18:17:47 +0100

    Bug canvi títol i autor

--- FONTS/main/domain/documents/DocumentsSet.java
+++ FONTS/main/domain/documents/DocumentsSet.java
@@ -192,10 +192,10 @@ public class DocumentsSet {
     public void updateTitleAndAuthorDocument(String oldTitle, String oldAuthor, String newTitle, String newAuthor) throws ExceptionNoDocument, ExceptionDocumentExists {
         if (oldTitle != newTitle || oldAuthor != newAuthor) {       // Si no ha canviat ni el títol ni l'autor, no cal fer res
             Document document = getDocument(oldTitle, oldAuthor);
-            deleteDocument(oldTitle, oldAuthor);        // Esborrem el document del conjunt
             document.setTitle(newTitle);
             document.setAuthor(newAuthor);
             registerDocument(document);                 // El tornem a afegir amb els nous títol i autor
+            deleteDocument(oldTitle, oldAuthor);        // Esborrem el document del conjunt
         }
     }
 
